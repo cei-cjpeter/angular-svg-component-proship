f7bb3074f987c32011eef1a2f8e321bd2e896ede
cei-cjpeter <pchandran@ceiamerica.com>
2021-12-09 16:14:31 +0530

    Initial commit

---
+++ src/app/proship-icon/proship-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proship-icon',
+  templateUrl: './proship-icon.component.svg',
+  styleUrls: ['./proship-icon.component.css']
+})
+export class ProshipIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
